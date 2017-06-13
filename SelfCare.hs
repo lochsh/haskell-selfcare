877f633b02c90cb9e30c8896b799cfdbf6fe49a4
Hannah McLaughlin <h@mcla.ug>
2017-06-13 23:19:55 +0100

    Working with randomness, hooray

--- SelfCare.hs
+++ SelfCare.hs
@@ -1,15 +1,20 @@
 import System.IO
+import System.Random (randomRIO)
 
 -- Loads lines from a text file into a list
 getLines :: FilePath -> IO [String]
 getLines path = do contents <- readFile path
                    return (lines contents)
 
+-- Select random element from list
+randomPick :: [a] -> IO a
+randomPick xs = fmap (xs !!) $ randomRIO (0, length xs - 1)
+
 main = do
     nouns <- getLines "nouns.txt"
     adjs <- getLines "adjectives.txt"
 
-    let noun = head nouns
-    let adj = head adjs
+    noun <- randomPick nouns
+    adj <- randomPick adjs
 
     putStr $ "You are a" ++ adj ++ " " ++ noun
